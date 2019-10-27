5021963ff5dfa7a6e543ce3f67ba4e69f1e69890
Malu Mantovanelli <mlguimaraess@gmail.com>
2019-10-27 20:00:02 -0300

    restructuring components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { InMemoryDataService } from "./services/in-memory-data.service";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { MessagesComponent } from "./messages/messages.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { MessagesComponent } from "./components/messages/messages.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HeroSearchComponent } from "./components/hero-search/hero-search.component";
 
 @NgModule({
   declarations: [
